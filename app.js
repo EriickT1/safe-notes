e45a53258b25f6fc895c9f68e6cff81983a4a61a
EriickT1 <eriick1324@gmail.com>
2025-06-17 22:24:48 -0400

    Cambios app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
- res.send('<h1>Notas seguras (o no tan seguras)</h1>');
+ res.send('<h1>Notas seguras (o no tan seguras) ;) </h1>');
 });
 app.listen(3000, () => {
  console.log('SafeNotes corriendo en http://localhost:3000');
